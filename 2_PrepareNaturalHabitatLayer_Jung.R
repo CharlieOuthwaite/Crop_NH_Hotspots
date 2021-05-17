da5ff99d2133a77f1cbcfc797275701fb33dab3a
CharlieOuthwaite <charloteouthwaite@gmail.com>
2021-05-17 09:57:28 +0100

    Using alternative NH data

---
+++ 2_PrepareNaturalHabitatLayer_Jung.R
@@ -0,0 +1,144 @@
+
+##%######################################################%##
+#                                                          #
+####           Preparing natural habitat data           ####
+#                                                          #
+##%######################################################%##
+
+# this version of the script is using the Jung et al 2020 paper
+
+rm(list = ls())
+
+# load libraries
+library(raster)
+library(ggplot2)
+
+# set directories
+dataDir <- "0_data/"
+dataDir2 <- "1_PrepareCropLayers/"
+outDir <- "2_PrepareNaturalHabitatLayer/"
+dir.create(outDir)
+
+# take a look at the habitat files
+files <- list.files(paste0(dataDir, "/lvl1_frac_1km_ver004/lvl1_frac_1km_ver004"))
+files
+
+# create a raster stack of the natural habitat layers
+# 12 maps
+hab_maps <- stack(paste0(dataDir, "/lvl1_frac_1km_ver004/lvl1_frac_1km_ver004/", files))
+
+# which are the NH ones in files: 1, 6,7,8, 9
+# (10 and 11 = rocky and desert?)
+# remove the non-natural habitat ones
+hab_maps <- hab_maps[[c(1,6,7,8,9)]]
+
+names(hab_maps)
+
+# information on Zenodo says the map values are fractional * 1000, 
+# so dividing here to get the original values. 
+hab_maps <- hab_maps/1000
+
+# sum the rasters to get the total fractional cover of natural habitat
+frac_NH <- sum(hab_maps, na.rm = T) 
+
+plot(frac_NH)
+
+# save the fractional NH map
+writeRaster(x = frac_NH,filename = paste(outDir, "Fractional_NH_Jung.tif",sep=""), format="GTiff")
+
+
+# frac_NH <- raster(paste(outDir, "Fractional_NH_Jung.tif",sep=""))
+
+
+#### Just look at NH in area where crops grown ####
+
+
+
+## use the binary crop map as a mask to just look at NH in areas where crops are grown
+
+# load the binary crop map
+CropDist <- raster(paste0(dataDir2, "CroplandBinary.tif"))
+
+
+# when trying to reproject as below, get an error: 
+# Error in if (maxy == miny) { : missing value where TRUE/FALSE needed
+# if you cut the edges this error does not occur
+# https://gis.stackexchange.com/questions/220589/error-using-projectraster-in-r-error-in-if-maxy-miny-missing-value-whe
+extent(CropDist) <- c(xmin= -17372530, xmax= 17372470, ymin= 0.99*(-6357770), ymax= 0.99*(7347230))
+
+# try cropping the extent rather than the above
+b <- extent(-17372530, 17372470,  0.99*(-6357770), 0.99*(7347230))
+
+# reproject the binary crop map to match up with the NH map
+CropDist_reproj <- projectRaster(CropDist, frac_NH)
+
+
+# creates a map of the natural habitat values in just the areas of cropland
+NatHabCrop <- raster::mask(x = frac_NH, mask = CropDist_reproj)
+
+
+# save the NH in cropland map
+writeRaster(x = NatHabCrop,filename = paste(outDir, "NH_Cropland_Area_Jung.tif",sep=""), format="GTiff")
+
+
+
+### create a figure of NH in cropland values that are binned. ###
+
+# need to reproject for mapping with country polygons
+crs(NatHabCrop)
+
+
+### create a plot with binned values for prop NH ###
+
+# aggregating for faster plotting
+NatHabCrop2 <- aggregate(NatHabCrop, fact = 10)
+
+# convert to data frame for use with ggplot
+plot_data <- as.data.frame(NatHabCrop2, xy = TRUE)
+
+# remove NAs
+plot_data <- plot_data[!is.na(plot_data$Fractional_NH_Jung), ]
+
+# multiply by 10 to get actual % values
+plot_data$Fractional_NH_Jung <- plot_data$Fractional_NH_Jung*100
+
+# organise the percNH info into bins
+plot_data$bin <- NA
+
+plot_data[plot_data$Fractional_NH_Jung >= 0 & plot_data$Fractional_NH_Jung <= 20, 'bin'] <- "0 - 20%"
+plot_data[plot_data$Fractional_NH_Jung > 20 & plot_data$Fractional_NH_Jung <= 40, 'bin'] <- "21 - 40%"
+plot_data[plot_data$Fractional_NH_Jung > 40 & plot_data$Fractional_NH_Jung <= 60, 'bin'] <- "41 - 60%"
+plot_data[plot_data$Fractional_NH_Jung > 60 & plot_data$Fractional_NH_Jung <= 80, 'bin'] <- "61 - 80%"
+plot_data[plot_data$Fractional_NH_Jung > 80, 'bin'] <- "81 - 100%"
+
+plot_data$bin <- as.factor(plot_data$bin)
+
+
+# load world map outline
+map.world <- map_data("world")
+
+#library(maptools)
+#data(wrld_simpl)
+
+
+# plot
+ggplot() +
+  geom_tile(data = plot_data, aes(x = x, y = y, fill = bin), na.rm = TRUE, alpha = 0.7) +
+  scale_fill_manual(values = c("#EE0000", "#EE7600", "#EEC900", "#66CD00", "#458B00")) +
+  geom_map(data=map.world, map=map.world,
+           aes(x=long, y=lat, group=group, map_id=region),
+           fill= "transparent", colour="lightgrey", size=0.2) +
+  theme_bw() +
+  theme(axis.title = element_blank(), 
+        plot.background = element_blank(), 
+        panel.background = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        legend.title = element_blank(),
+        panel.grid = element_blank(), 
+        legend.position = "bottom")  
+
+ggsave(filename = paste0(outDir, "/Fugure1_Map_crop_NH_Jung.pdf"), height = 6, width = 8)
+
+
+
--- 5_Models_Projections.R
+++ 5_Models_Projections.R
@@ -35,12 +35,11 @@ pred.sites.metrics <- SiteMetrics(predicts, extra.cols = c("Predominant_land_use
 
 ### only interested in natural habitats plus cropland, drop other land uses ###
 
-# site level data cropland only
+# site level data primary, secondary and cropland only
 sites.sub <- pred.sites.metrics[!pred.sites.metrics$Predominant_land_use %in% c("Urban", "Pasture", "Cannot decide", "Plantation forest"), ]
 
 # remove sites with NA in lat/long columns
 sites.sub <- sites.sub[!is.na(sites.sub$Longitude),  ] # 15612 rows
-
 
 
 
@@ -51,27 +50,43 @@ sites.sub <- sites.sub[!is.na(sites.sub$Longitude),  ] # 15612 rows
 #                                                          #
 ##%######################################################%##
 
-# using reprojected percentage natural habitat raster based on Hoskins et al 2016:
+# use Jung habitat data, combining natural habitat types 
 
-# Hoskins, A.J., Bush, A., Gilmore, J., Harwood, T., Hudson, L.N., Ware, C., Williams, 
-# K.J., and Ferrier, S. (2016). Downscaling land-use data to provide global estimates 
-# of five land-use classes. Ecol. Evol.
+# # read in teh fractional natural habitat data
+NatHabCrop <- raster(paste0("2_PrepareNaturalHabitatLayer/Fractional_NH_Jung.tif"))
 
-# read in the raster
-percNH <- raster(paste0(datadir,"/PercentNatural.tif"))
+# convert to actual % values
+NatHabCrop <- NatHabCrop*100
 
-# 
-percNH <- percNH/10
+plot(NatHabCrop)
+
+# convert the PREDICTS lat/longs into spatial points
+sites.sub_xy <- sites.sub[, c("Longitude", "Latitude")]
+sites.sub_xy <- SpatialPoints(sites.sub_xy, proj4string = CRS("+init=epsg:4326 +proj=longlat +ellps=WGS84"))
+
 
 # extract the dataset info for the PREDICTS sites
-sites.sub$percNH <- extract(percNH, sites.sub_xy, na.rm = FALSE)
+sites.sub$percNH <- extract(NatHabCrop, sites.sub_xy, na.rm = FALSE)
 
 # how many NAs
-nrow(sites.sub[is.na(sites.sub$percNH),]) #29
+nrow(sites.sub[is.na(sites.sub$percNH),]) #0
 
 
+# remove any rows that have NA in the variable columns
+summary(is.na(sites.sub))
+
+# nrows of dataset
+nrow(sites.sub) # 15612
+
+# remove those sites that have "Cannot decide" as a use intensity
+nrow(sites.sub[sites.sub$Use_intensity == "Cannot decide", ]) # 1946
+sites.sub <- sites.sub[!sites.sub$Use_intensity == "Cannot decide", ] 
+
+nrow(sites.sub) # 13666
 
 
+# drop unused levels of factors
+sites.sub <- droplevels(sites.sub)
 
 ##%######################################################%##
 #                                                          #
@@ -81,89 +96,207 @@ nrow(sites.sub[is.na(sites.sub$percNH),]) #29
 
 
 
-# remove any rows that have NA in the variable columns
-summary(is.na(sites.sub))
-
-# remove rows with NAs for any variable of interest
-sites.sub <- sites.sub[!is.na(sites.sub$homogen), ] # 11934
-sites.sub <- sites.sub[!is.na(sites.sub$Hansen_mindist), ] # 11796
-sites.sub <- sites.sub[!is.na(sites.sub$percNH), ] # 11782
-
-# nrows of dataset
-nrow(sites.sub) # 11782
-
-# remove those sites that have "Cannot decide" as a use intensity
-nrow(sites.sub[sites.sub$Use_intensity == "Cannot decide", ]) # 1389
-sites.sub <- sites.sub[!sites.sub$Use_intensity == "Cannot decide", ] 
-
-nrow(sites.sub) # 10393
-
-
-
-##%######################################################%##
-#                                                          #
-####                Data transformations                ####
-#                                                          #
-##%######################################################%##
-
-final.data.trans <- final.data
-
-# standardise all continuous variables
-final.data.trans$percNH <-scale(final.data.trans$percNH)
-
-# get data sections for the scaling info for plotting later
-percNH <- final.data.trans$percNH
-
-p <- c("percNH", attr(percNH, "scaled:scale"), attr(percNH, "scaled:center"))
-
-values <- rbind(d, f, l, h, p)
-colnames(values) <- c("variable", "scale", "centre")
-
-#save
-write.csv(values, paste0(outdir, "/Scaling_values.csv"), row.names = FALSE)
-
-
-### organise factor levels ###
-
-# drop unused levels of factors
-final.data.trans <- droplevels(final.data.trans)
-
-# set factor levels in the best order
-final.data.trans$Use_intensity <- relevel(final.data.trans$Use_intensity, ref = "Minimal use")
-
 # nsites per use intensity
-table(final.data.trans$Use_intensity)
+table(sites.sub$Use_intensity)
+table(sites.sub$Predominant_land_use)
 
 
 # set land use as character variable
-final.data.trans$Predominant_land_use <- as.character(final.data.trans$Predominant_land_use)
+sites.sub$Predominant_land_use <- as.character(sites.sub$Predominant_land_use)
 
 
 # combine secondary land uses
-final.data.trans$Predominant_land_use <- sub("Mature secondary vegetation", "Secondary vegetation", final.data.trans$Predominant_land_use)
-final.data.trans$Predominant_land_use <- sub("Intermediate secondary vegetation", "Secondary vegetation", final.data.trans$Predominant_land_use)
-final.data.trans$Predominant_land_use <- sub("Young secondary vegetation", "Secondary vegetation", final.data.trans$Predominant_land_use)
-final.data.trans[final.data.trans$Predominant_land_use == "Secondary vegetation (indeterminate age)", 'Predominant_land_use'] <- "Secondary vegetation"
+sites.sub$Predominant_land_use <- sub("Mature secondary vegetation", "Secondary vegetation", sites.sub$Predominant_land_use)
+sites.sub$Predominant_land_use <- sub("Intermediate secondary vegetation", "Secondary vegetation", sites.sub$Predominant_land_use)
+sites.sub$Predominant_land_use <- sub("Young secondary vegetation", "Secondary vegetation", sites.sub$Predominant_land_use)
+sites.sub[sites.sub$Predominant_land_use == "Secondary vegetation (indeterminate age)", 'Predominant_land_use'] <- "Secondary vegetation"
 
-table(final.data.trans$Predominant_land_use)
+table(sites.sub$Predominant_land_use)
 
 
 # set factor levels of predominant land use
-final.data.trans$Predominant_land_use <- factor(final.data.trans$Predominant_land_use,
+sites.sub$Predominant_land_use <- factor(sites.sub$Predominant_land_use,
                                                 levels=c("Primary vegetation","Secondary vegetation", "Cropland"))
 
 
 # nsites per land use
-table(final.data.trans$Predominant_land_use)
+table(sites.sub$Predominant_land_use)
 
 
-
+sites.sub$LogAbun <- log(sites.sub$Total_abundance + 1)
 
 # save transformed dataset
-save(final.data.trans, file = paste0(outdir, "/PREDICTS_dataset_inc_variables_TRANS.rdata"))
+save(sites.sub, file = paste0(outdir, "PREDICTS_subset.rdata"))
 
 
 
 
 
+##%######################################################%##
+#                                                          #
+####                 Run simple models                  ####
+#                                                          #
+##%######################################################%##
 
+
+
+# remove NAs in the specified columns
+model_data <- na.omit(sites.sub[,c('Species_richness','Predominant_land_use','Use_intensity','percNH','SS','SSB','SSBS')])
+
+# summaries
+length(unique(model_data$SS)) # 577
+length(unique(model_data$SSBS)) # 13666
+
+
+# run set of simple models with different fixed effects structures
+
+srmod <- GLMERSelect(modelData = model_data, 
+                     responseVar = "Species_richness",
+                     fitFamily = "poisson", 
+                     fixedFactors = c("Predominant_land_use", "Use_intensity"),
+                     fixedTerms = list(percNH = 1),
+                     randomStruct = "(1|SS)+(1|SSB)+(1|SSBS)", 
+                     fixedInteractions = c("Predominant_land_use:poly(percNH,1)",
+                                           "Use_intensity:poly(percNH,1)",
+                                           "Predominant_land_use:Use_intensity"), verbose = F)
+
+summary(srmod$model)
+# model selected:
+# Species_richness ~ Predominant_land_use + Use_intensity + poly(percNH, 1) + Use_intensity:poly(percNH, 1) + Predominant_land_use:Use_intensity +  
+# (1 | SS) + (1 | SSB) + (1 | SSBS)
+
+sr1 <- GLMER(modelData = model_data,responseVar = "Species_richness",fitFamily = "poisson",
+             fixedStruct = "Predominant_land_use + Use_intensity + poly(percNH, 1) + Use_intensity:poly(percNH, 1) + Predominant_land_use:Use_intensity",randomStruct = "(1|SS)+(1|SSB)+(1|SSBS)",REML = TRUE)
+
+summary(sr1$model)
+
+#Warning message:
+# In checkConv(attr(opt, "derivs"), opt$par, ctrl = control$checkConv,  :
+#               Model failed to converge with max|grad| = 0.0041052 (tol = 0.001, component 1)
+
+
+
+#### ABUNDANCE MODELS ####
+
+model_data <- na.omit(sites.sub[,c('LogAbun','Predominant_land_use','Use_intensity','percNH','SS','SSB','SSBS')])
+
+# summaries
+length(unique(model_data$SS)) # 510
+length(unique(model_data$SSBS)) # 11396
+
+
+abmod <- GLMERSelect(modelData = model_data, 
+                     responseVar = "LogAbun",
+                     fitFamily = "gaussian", 
+                     fixedFactors = c("Predominant_land_use", "Use_intensity"),
+                     fixedTerms = list(percNH = 1),
+                     randomStruct = "(1|SS)+(1|SSB)", 
+                     fixedInteractions = c("Predominant_land_use:poly(percNH,1)",
+                                           "Use_intensity:poly(percNH,1)",
+                                           "Predominant_land_use:Use_intensity"), verbose = F)
+
+summary(abmod$model)
+# model selected:
+# LogAbun ~ Predominant_land_use + Use_intensity + Predominant_land_use:Use_intensity + (1 | SS) + (1 | SSB)
+
+
+## no significant effect of percNH or interactions
+
+# run set of simple models with different fixed effects structures
+#ab1 <- GLMER(modelData = model_data,responseVar = "LogAbun",fitFamily = "gaussian",
+#             fixedStruct = "Predominant_land_use + Use_intensity + percNH_rs + Predominant_land_use*Use_intensity",randomStruct = "(1|SS)+(1|SSB)",REML = TRUE)
+
+
+#summary(ab1$model)
+
+
+
+### Predict responses for plotting ###
+
+
+
+# basic table of median values and reference factors
+pred_tab <- data.frame(percNH = NA,
+                       Use_intensity = "Minimal use",
+                       Predominant_land_use = "Primary vegetation",
+                       Species_richness = 0,
+                       LogAbun = 0)
+
+levels(pred_tab$Predominant_land_use) <- levels(sr1$data$Predominant_land_use)
+levels(pred_tab$Use_intensity) <- levels(sr1$data$Use_intensity) 
+
+vals <- 1000
+
+pred_tab2 <- do.call("rbind", replicate(vals, pred_tab, simplify = FALSE))
+
+pred_tab2$percNH_rs <- seq(from = 0, to = 100, length.out = 1000)
+
+# rescale the values
+pred_tab2$percNH_rs <-(pred_tab2$percNH_rs-values[, "centre"])/values[, "scale"]
+
+
+# predict the result
+result <- PredictGLMER(model = sr1$model, data = pred_tab2, se.fit = TRUE, seMultiplier = 1.96)
+
+# transform the results
+result <- exp(result)
+
+vals <- seq(from = 0, to = 100, length.out = 1000)
+
+
+
+# SR plot = full range
+ggplot(data = result) +
+  geom_line(aes(x = vals, y = y), col = c("#66CD00")) +
+  geom_ribbon(aes(x = seq(from = 0, to = 100, length.out = 1000), ymin= yminus, ymax = yplus), fill = c("#66CD00"), alpha = 0.3) +
+  geom_rug(data = final.data.trans, aes(x = percNH), size = 0.1) +
+  ylim(c(0,30)) +
+  xlim(c(0, 100)) +
+  xlab("Percentage of Natural Habitat") +
+  ylab("Species Richness") +
+  theme_bw() +
+  theme(panel.grid = element_blank(),
+        legend.position = c(0.8,0.8), legend.title = element_blank(),
+        aspect.ratio = 1) 
+
+ggsave(filename = paste0(outdir, "/SR_simplemod_percNH_plot.png"))
+
+### abundance model ###
+
+# predict the result
+result2 <- PredictGLMER(model = ab1$model, data = pred_tab2[,c(1:3,5)], se.fit = TRUE, seMultiplier = 1.96)
+
+# transform the results
+result2 <- exp(result2)-1
+
+vals <- seq(from = 0, to = 100, length.out = 1000)
+
+# SR plot = full range
+ggplot(data = result2) +
+  geom_line(aes(x = vals, y = y), col = c("#66CD00")) +
+  geom_ribbon(aes(x = vals, ymin= yminus, ymax = yplus), fill = c("#66CD00"), alpha = 0.3) +
+  geom_rug(data = final.data.trans, aes(x = percNH), size = 0.1) +
+  ylim(c(0,300)) +
+  xlim(c(0, 100)) +
+  xlab("Percentage of Natural Habitat") +
+  ylab("Total abundance") +
+  theme_bw() +
+  theme(panel.grid = element_blank(),
+        legend.position = c(0.8,0.8), legend.title = element_blank(),
+        aspect.ratio = 1) 
+
+ggsave(filename = paste0(outdir, "/AB_simplemod_percNH_plot.png"))
+
+
+
+
+
+##%######################################################%##
+#                                                          #
+####                    Model checks                    ####
+#                                                          #
+##%######################################################%##
+
+
+# check out the performance package
+library(performance)
